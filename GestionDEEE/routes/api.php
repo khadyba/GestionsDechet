909bfb8af6ac4ed294c704950e11e363324bf1e6
Ssada922 <sadandiaye156@gmail.com>
2025-02-22 19:24:00 +0100

    Api Gestion: Appareil, Signalement

--- GestionDEEE/routes/api.php
+++ GestionDEEE/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Routes Gestion des Appareils
+Route::get('/device/index', [DeviceController::class, 'index']);
+Route::post('/device/store', [DeviceController::class, 'store']);
+Route::get('/device/show/{id}', [DeviceController::class, 'show']);
+Route::put('/device/update/{id}', [DeviceController::class, 'update']);
+Route::delete('/device/destroy/{id}', [DeviceController::class, 'destroy']);
+
+// Routes Gestion de Signelement
+Route::get('/report/index', [ReportController::class, 'index']);
+Route::post('/report/store', [ReportController::class, 'store']);
+Route::get('/report/show/{id}', [ReportController::class, 'show']);
+Route::put('/report/update/{id}', [ReportController::class, 'update']);
+Route::delete('/report/destroy/{id}', [ReportController::class, 'destroy']);
+
